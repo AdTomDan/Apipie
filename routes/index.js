f9b26ba227564e2734e7895d89ec274316b3b57f
Tom Adams <thomasjeadams@btinternet.com>
2021-11-19 17:09:26 +0100

    started readme file

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,24 @@ const User = require("../models/User.model")
 const Api = require("../apis/api")
 
 /* GET home page. */
-router.get('/', (req, res)=> {
+router.route("/")
+.get('/', (req, res)=> {
   res.render("login")
-});
+})
+.post((req, res)=>{
+  const {username,password} = req.body
+	if(!username||!password) res.render('login',{errorMessage:"Username/password not provided."})
+	User.findOne({username})
+	.then((user)=>{
+		if(!user) res.render('login',{errorMessage:"Username does not exist."})
+		const isPwdCorrect = bcrypt.compareSync(password,user.password)
+		if(isPwdCorrect) {
+			req.session.loggedInUser = user
+			res.render("user-profile")
+		} 
+		else res.render('login',{errorMessage:"Username/password incorrect."})
+	})
+})
 
 /* GET from API */
 router.get('/api', (req, res)=> {
