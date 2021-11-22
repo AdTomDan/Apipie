b36bd5d1b75ce09c054e13b59717ad5f84de7340
Tom Adams <thomasjeadams@btinternet.com>
2021-11-22 11:36:07 +0100

    Added like button for posts and it displays who liked it, but not the like count

--- routes/feed.routes.js
+++ routes/feed.routes.js
@@ -4,26 +4,34 @@ var router = express.Router();
 const User = require("../models/User.model")
 const Post = require("../models/Post.model")
 
-router.route("/")
+
+router.route("/post/like/:id")
 .get(async(req,res)=>{
-    const allPosts = await Post.find().populate("user", "username").sort({'createdAt': -1})
-    res.render("feed/feed",{allPosts})
+    const id = req.params.id
+    const currentUser = req.session.loggedInUser
+    const likedPost = await Post.findByIdAndUpdate(id,{likes:currentUser})
+    res.redirect("/feed")
 })
 
 router.route("/post")
 .post(async(req,res)=>{
     try {
         const {text,image} = req.body
-
+        
         const newPost = await (await Post.create({user: req.session._id,text,image,likes:null,likeCount:0,comments:null}))
-        console.log("new post is: ",newPost)
-
-        const allPosts = await Post.find().populate("user", "username").sort({'createdAt': -1})
-        console.log(allPosts)
-        res.render("feed/feed",{allPosts})
+        const allPosts = await Post.find().populate("user", "username").populate("likes", "username").sort({'createdAt': -1})
+        const currentUser = req.session.loggedInUser
+        res.render("feed/feed",{allPosts, currentUser})
     } catch (err) {
         console.log(err)
     }
+})
+
+router.route("/")
+.get(async(req,res)=>{
+    const allPosts = await Post.find().populate("user", "username").populate("likes", "username").sort({'createdAt': -1})
+    const currentUser = req.session.loggedInUser
+    res.render("feed/feed",{allPosts, currentUser})
 })
 
 module.exports = router;
